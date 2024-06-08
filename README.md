4082de687b8121c85901ea30a478b0f5d6640017
acharyarupak391 <acharyarupak391@gmail.com>
2024-06-08 20:53:19 +0545

    Updated README, package.json scripts, removed unnecessary code & added .env.example file for client

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
-# Node.js template
+# Realtime Settlement Process w/ Websockets
 
-This is a Node.js project.
+This repository contains a server and client implementation of a realtime settlement process using websockets. 
+The server is written in NodeJS(express & express-ws) and the client is written in NextJS. 
+The server is responsible for handling the connection pools for two parties, A and B and the settlement process between them.
+The client is responsible for connecting to the server and sending settlement requests from party A and B.
 
-Add your [configuration](https://codesandbox.io/docs/projects/learn/setting-up/tasks) to optimize it for [CodeSandbox](https://codesandbox.io/p/dashboard).
+## How to run the project
 
-## Resources
+- Clone the repository
+- Run `yarn setup` in the root directory to install all dependencies for the server and client
 
-- [CodeSandbox — Docs](https://codesandbox.io/docs/learn)
-- [CodeSandbox — Discord](https://discord.gg/Ggarp3pX5H)
+- Running the server
+  - Run `yarn start:server` to start the server 
+    - *(Or `yarn dev:server` to start the server in development mode)*
+  - You can visit `http://localhost:8080` in your browser to verify that the server is running
+
+- Running the client
+  - Make sure the server is running
+  - Run `yarn start:client` to start the client 
+    - *(Or `yarn dev:client` to start the client in development mode)*
+  - You can visit `http://localhost:3000` in your browser to verify that the client is running
+    - *(By default, the client tries to connect to the server at `ws://localhost:8080` for websocket communication. If you are running the server on a different port or different host, you must specify the server address in the `.env` file inside client directory as `NEXT_PUBLIC_WS_URL`)*
--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
-import express, { Request, Response } from "express";
+import express, { Response } from "express";
 import expressWs from "express-ws";
 import { initializeDatabase } from "./db";
-import { addNewConnection } from "./db/utils";
-import { SENDER } from "./types"
 import { setupWSRoutes } from "./ws/routes";
-import { isValidConnection } from "./ws/utils";
 
 const expressApp = express();
 const port = 8080;
@@ -14,47 +11,12 @@ const { app } = expressWs(expressApp);
 // Middleware to parse JSON
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Hello from Node.js!");
+app.get("/", (_, res: Response) => {
+  res.status(200).send("Hello from the server side!");
 });
 
 (async () => {
   const db = await initializeDatabase();
-
-  app.get("/new", async (_, res: Response) => {
-    try {
-      const connectionId = await addNewConnection(db, SENDER.PARTY_A);
-      res.status(200).json({ connectionId });
-    } catch (_) {
-      res.status(500).json({ error: "Something went wrong!" });
-    }
-  });
-
-  app.get("/join/:connectionId", async (req: Request, res: Response) => {
-    const { connectionId } = req.params;
-
-    try {
-      const valid = await isValidConnection(connectionId, db)
-
-      if (!valid) {
-        res.status(400).json({ error: "Connection not available!" });
-        return;
-      }
-
-      res.status(200).json({ message: "Connection updated!" });
-    } catch (_) {
-      res.status(500).json({ error: "Something went wrong!" });
-    }
-  })
-
-  app.get("/test", async (_, res: Response) => {
-    try {
-      const connections = await db.all("SELECT * FROM connections");
-      res.json({ connections });
-    } catch (_) {
-      res.json({ error: "Something went wrong!" });
-    }
-  });
 
   setupWSRoutes(app, db);
 })();
